dbb3f9685525338e6d407dc0f3331377e396fcfa
ChifaMaster <ekain.bernabe@maristak.net>
2021-10-27 10:28:48 +0200

    Tarjeta para el reto

--- Tarjeta/app/src/main/java/com/example/tarjeta/MainActivity.java
+++ Tarjeta/app/src/main/java/com/example/tarjeta/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tarjeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,28 +11,68 @@ import android.widget.Toast;
 import com.example.tarjeta.interfaces.TarjetaAPI;
 import com.example.tarjeta.models.Tarjeta;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.BreakIterator;
+import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView num_tarjeta;
-    Button boton_ver;
+    Button ver_tarjeta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         num_tarjeta = findViewById(R.id.num_tarjeta);
-        boton_ver = findViewById(R.id.boton_ver);
-        find();
+        ver_tarjeta = findViewById(R.id.ver_tarjeta);
+
+        ver_tarjeta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mensaje = getString("http://10.122.27.162:4000/infocards2/user1/");
+                num_tarjeta.setText(mensaje);
+            }
+        });
+
+
 
     }
+    public static  String getString(String path) {
+        URL url;
+        OkHttpClient client = new OkHttpClient();
 
-    private void find(){
-        Retrofit retrofit= new Retrofit.Builder().baseUrl("http://10.122.27.162:4000/infocards2/user1").build();
-        num_tarjeta.setText(retrofit.toString());
+        try {
+            url = new URL(path);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                Log.e("Mensaje",response.body().string());
+                return response.body().string();
+            }
+        } catch (Exception ex) {
+            return null;
+        }
     }
+
+
 }
--- Tarjeta/app/src/main/java/com/example/tarjeta/interfaces/TarjetaAPI.java
+++ Tarjeta/app/src/main/java/com/example/tarjeta/interfaces/TarjetaAPI.java
@@ -2,12 +2,14 @@ package com.example.tarjeta.interfaces;
 
 import com.example.tarjeta.models.Tarjeta;
 
+import java.util.List;
+
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 public interface TarjetaAPI {
-    @GET("infocards2/{user1}")
-    public Call<Tarjeta> find(@Path("user1") String user);
+    @GET("infocards2/user1/")
+    Call<String> getStringScalar(@Body String body);
 }
